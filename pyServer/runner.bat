deaa7285514b7fe085cc081bff94c3b23e5359a6
CharudattaB <132000472+CharudattaB@users.noreply.github.com>
2025-01-29 22:29:33 +0530

    Update runner.bat

--- pyServer/runner.bat
+++ pyServer/runner.bat
@@ -3,13 +3,13 @@ set APP_FILE=app.py
 set APP_PORT=5000
 set CLOUDFLARED_PATH=cloudflared.exe
 
-:: Check if cloudflared exists
-if not exist "%CLOUDFLARED_PATH%" (
-    echo cloudflared.exe not found. Please download it from:
-    echo https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/installation/
-    pause
-    exit /b
-)
+#:: Check if cloudflared exists
+#if not exist "%CLOUDFLARED_PATH%" (
+#    echo cloudflared.exe not found. Please download it from:
+#    echo https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/installation/
+#    pause
+#    exit /b
+#)
 
 :: Start the Python app
 echo Starting %APP_FILE%...
